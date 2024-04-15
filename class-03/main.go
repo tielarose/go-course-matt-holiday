3c10641c6d7f85bc20ed4f50bc86f7dde4786348
Tiela Black-Law <tiela.rose@gmail.com>
2024-04-14 21:41:48 -0700

    Class 03 - Ints and Floats in go

---
+++ class-03/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main(){
+	var sum float64
+	var n int
+
+	for {
+		var val float64
+
+		if _, err := fmt.Fscanln(os.Stdin, &val); err != nil {
+			break
+		}
+
+		sum += val
+		n++
+	}
+
+	if n == 0 {
+		fmt.Fprintln(os.Stderr, "no values")
+		os.Exit(-1)
+	}
+
+	fmt.Println("The average is", sum/float64(n))
+}
