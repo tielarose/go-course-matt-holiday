406beb1741b980870b51f04cb7087317bb3e9ff4
Tiela Rose Black-Law <26930264+tielarose@users.noreply.github.com>
2024-04-15 04:02:46 +0000

    Class 02 - Say Hello program with unit tests

--- cmd/main.go
+++ cmd/main.go
@@ -7,9 +7,5 @@ import (
 )
 
 func main() {
-	if len(os.Args) > 1 {
-        fmt.Println(hello.Say(os.Args[1]))
-    } else {
-        fmt.Println(hello.Say("world"))
-    }
+    fmt.Println(hello.Say(os.Args[1:]))
 }
--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,38 @@ package hello
 import "testing"
 
 func TestSayHello(t *testing.T){
-    want := "Hello, test!!!!"
-    got := Say("test")
+    // want := "Hello, test!"
+    // got := Say([]string{"test"})
 
-    if want != got {
-        t.Errorf("wanted %s, got %s", want, got)
+    // if want != got {
+    //     t.Errorf("wanted %s, got %s", want, got)
+    // }
+
+    // subtests is a list of dictionaries
+    // each dict:
+    //    items: list of names
+    //    result: what we'd expect the result string to be
+    
+    subtests := []struct{
+        items []string
+        result string
+    }{
+        {
+            result: "Hello, world!",
+        },
+        {
+            items: []string{"Tiela"},
+            result: "Hello, Tiela!",
+        },
+        {
+            items: []string{"Tiela", "Pati", "Joseph"},
+            result: "Hello, Tiela, Pati, Joseph!",
+        },
+    }
+
+    for _, st := range subtests {
+        if s := Say(st.items); s != st.result {
+            t.Errorf("wanted %s (%v), got %s", st.result, st.items, s)
+        }
     }
 }
