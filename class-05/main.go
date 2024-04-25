d534cc69221c71157b0217196d79691c25c1bf43
Tiela Black-Law <tiela.rose@gmail.com>
2024-04-24 21:44:56 -0700

    Class 05 - Arrays, Slices and Maps

---
+++ class-05/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+func main() {
+	scan := bufio.NewScanner(os.Stdin)
+	words := make(map[string]int)
+
+	scan.Split(bufio.ScanWords)
+
+	for scan.Scan() {
+		words[scan.Text()]++
+	}
+
+	fmt.Println(len(words), "unique words")
+
+	type kv struct {
+		key string
+		val int
+	}
+
+	var ss []kv
+
+	for k, v := range words {
+		ss = append(ss, kv{k, v})
+	}
+
+	sort.Slice(ss, func(i, j int) bool {
+		return ss[i].val > ss[j].val
+	})
+
+	for _, s := range ss[:3] {
+		fmt.Println(s.key, "appears", s.val, "times")
+	}
+}
